0b3787db247b4f3fef39ffd48e35515b99d7f356
njdevengine <newjerseydevelopment@gmail.com>
2019-08-16 18:02:49 -0400

    Create linkedin_person_search.py

---
+++ linkedin_person_search.py
@@ -0,0 +1,26 @@
+from linkedin_api import Linkedin
+api = Linkedin('login email','Password123$$')
+
+search = api.search_people(
+  keywords='company name,sales manager,account executive',
+  industries=['43']
+)
+
+array = []
+for i in range(len(search)):
+    try:
+        if search[i]['distance'] == "DISTANCE_3":
+            pass
+        else:
+            print(search[i]['public_id'])
+            array.append((search[i]['public_id']))
+    except:
+        pass
+        
+array2 = []
+for i in array[:10]:
+    data = api.get_profile(i)
+    array2.append(data)
+    
+for i in array2:
+    print(i["firstName"],i["lastName"],i["experience"][0]["companyName"])#i["headline"])
